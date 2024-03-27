3852a52720497b4c3a48d7def6387dbc13776f89
Liam Nybro <liam.nybro@gmail.com>
2024-03-27 11:07:21 +0100

    hhh

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("toggle").addEventListener("click", function(){
-    console.log("funkar")
-    
+    console.log("funkar");
+    var faqTogla = document.getElementById("faq");
+    popup.classList.toggle("show");
 })
